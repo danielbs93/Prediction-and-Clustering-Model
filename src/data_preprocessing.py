60e90e8ac12c87253eb2acbca24ed18f291563dc
Daniel Ben Simon <49146507+danielbs93@users.noreply.github.com>
2020-06-10 15:39:54 +0300

    Add files via upload

---
+++ src/data_preprocessing.py
@@ -0,0 +1,9 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+import pandas as pd
+import numpy as np
+import matplotlib as mtl
+import statistics as st
